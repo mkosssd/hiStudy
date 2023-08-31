fc24afa97df2f97f043a6db2f458df74519b03c9
Manas Khandelwal <manas.khandelwal@sltech.local>
2023-08-31 18:55:14 +0530

    er

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-console.log(document.getElementById("brandName").innerText);
 let brands = ["HiStudy", "NoStudy", "LoStudy", "OkStudy"];
 setInterval(() => {
   let i = Math.floor(Math.random() * brands.length);
   document.getElementById("brandName").innerText = brands[i];
 }, 900);
+
+const swiper = document.querySelector(".scroll");
+const width = document.querySelector(".card").offsetWidth;
+
+function scroller(direction) {
+  if (direction === "right" ) {
+    swiper.scrollLeft += width + 40;
+  } else {
+    swiper.scrollLeft -= width + 40;
+  }
+  console.log(swiper.scrollLeft)
+}
