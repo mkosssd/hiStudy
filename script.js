d3389debb20030df28c41186b613e47577c42be5
Manas Khandelwal <manas.khandelwal@sltech.local>
2023-09-01 19:04:48 +0530

    sd

--- script.js
+++ script.js
@@ -8,10 +8,20 @@ const swiper = document.querySelector(".scroll");
 const width = document.querySelector(".card").offsetWidth;
 
 function scroller(direction) {
-  if (direction === "right" ) {
+  if (direction === "right") {
     swiper.scrollLeft += width + 40;
   } else {
     swiper.scrollLeft -= width + 40;
   }
-  console.log(swiper.scrollLeft)
+  console.log(swiper.scrollLeft);
 }
+let nav = document.getElementById("navBar");
+const toggle = () => {
+  if (nav.classList.contains("menu")) {
+    nav.classList.add("navs");
+    nav.classList.remove("menu");
+  } else {
+    nav.classList.add("menu");
+    nav.classList.remove("navs");
+  }
+};
