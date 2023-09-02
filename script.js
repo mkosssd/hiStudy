9d32b0d1de93985f096eed1489233e27a43850f0
Manas <manaskies19@gmail.com>
2023-09-02 23:28:39 +0530

    efr

--- script.js
+++ script.js
@@ -1,27 +1,56 @@
-let brands = ["HiStudy", "NoStudy", "LoStudy", "OkStudy"];
+let brands = ['HiStudy', 'NoStudy', 'LoStudy', 'OkStudy']
 setInterval(() => {
-  let i = Math.floor(Math.random() * brands.length);
-  document.getElementById("brandName").innerText = brands[i];
-}, 900);
+  let i = Math.floor(Math.random() * brands.length)
+  document.getElementById('brandName').innerText = brands[i]
+}, 900)
 
-const swiper = document.querySelector(".scroll");
-const width = document.querySelector(".card").offsetWidth;
+const swiper = document.querySelector('.scroll')
+const width = document.querySelector('.card').offsetWidth
 
-function scroller(direction) {
-  if (direction === "right") {
-    swiper.scrollLeft += width + 40;
+function scroller (direction) {
+  if (direction === 'right') {
+    swiper.scrollLeft += width + 40
   } else {
-    swiper.scrollLeft -= width + 40;
+    swiper.scrollLeft -= width + 40
   }
-  console.log(swiper.scrollLeft);
+  console.log(swiper.scrollLeft)
 }
-let nav = document.getElementById("navBar");
+let nav = document.getElementById('navBar')
 const toggle = () => {
-  if (nav.classList.contains("menu")) {
-    nav.classList.add("navs");
-    nav.classList.remove("menu");
+  if (nav.classList.contains('menu')) {
+    nav.classList.add('navs')
+    nav.classList.remove('menu')
   } else {
-    nav.classList.add("menu");
-    nav.classList.remove("navs");
+    nav.classList.add('menu')
+    nav.classList.remove('navs')
   }
-};
+}
+
+const counter = document.getElementById('counter')
+
+let count = 475
+let scrolling = false
+
+function update () {
+  counter.textContent = count
+}
+
+function isScroll () {
+  if (!scrolling && window.scrollY >= 200) {
+    scrolling = true
+    Counter()
+  }
+}
+
+function Counter () {
+  const interval = setInterval(() => {
+    if (count < 500) {
+      count++
+      update()
+    } else {
+      clearInterval(interval)
+    }
+  }, 40)
+}
+
+window.addEventListener('scroll', isScroll)
