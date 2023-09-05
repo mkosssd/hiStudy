e5e21ffe6853cc20734392ed0314f78cad9a90b3
Manas Khandelwal <manas.khandelwal@sltech.local>
2023-09-05 10:51:52 +0530

    final commit

--- script.js
+++ script.js
@@ -1,56 +1,79 @@
-let brands = ['HiStudy', 'NoStudy', 'LoStudy', 'OkStudy']
+let brands = ["HiStudy", "NoStudy", "LoStudy", "OkStudy","GoStudy"];
+let i = 0;
 setInterval(() => {
-  let i = Math.floor(Math.random() * brands.length)
-  document.getElementById('brandName').innerText = brands[i]
-}, 900)
-
-const swiper = document.querySelector('.scroll')
-const width = document.querySelector('.card').offsetWidth
-
-function scroller (direction) {
-  if (direction === 'right') {
-    swiper.scrollLeft += width + 40
-  } else {
-    swiper.scrollLeft -= width + 40
+  document.getElementById("brandName").innerText = brands[i];
+  i++;
+  if (i > brands.length-1 ) {
+    i = 0;
   }
-  console.log(swiper.scrollLeft)
+}, 900);
+
+const swiper = document.querySelector(".scroll");
+const width = document.querySelector(".cardBox").offsetWidth;
+console.log(width);
+function scroller(direction) {
+  if (direction === "right") {
+    swiper.scrollLeft += width;
+  } else {
+    swiper.scrollLeft -= width;
+  }
+  console.log(width);
 }
-let nav = document.getElementById('navBar')
+
+let nav = document.getElementById("navBar");
+let main = document.body;
 const toggle = () => {
-  if (nav.classList.contains('menu')) {
-    nav.classList.add('navs')
-    nav.classList.remove('menu')
+  if (nav.classList.contains("menu")) {
+    nav.classList.add("navs");
+    nav.classList.remove("menu");
+    main.classList.remove("blurBg");
   } else {
-    nav.classList.add('menu')
-    nav.classList.remove('navs')
+    main.classList.add("blurBg");
+    nav.classList.add("menu");
+    nav.classList.remove("navs");
   }
-}
+};
 
-const counter = document.getElementById('counter')
-
-let count = 475
-let scrolling = false
-
-function update () {
-  counter.textContent = count
-}
-
-function isScroll () {
-  if (!scrolling && window.scrollY >= 200) {
-    scrolling = true
-    Counter()
-  }
-}
-
-function Counter () {
+const counter = document.getElementById("counter");
+const counter2 = document.getElementById("counter2");
+const counter3 = document.getElementById("counter3");
+const counter4 = document.getElementById("counter4");
+let count = counter.innerText;
+let count2 = counter2.innerText;
+let count3 = counter3.innerText;
+let count4 = counter4.innerText;
+let scrolling = false;
+window.addEventListener("scroll", isScroll);
+function Counter() {
   const interval = setInterval(() => {
     if (count < 500) {
-      count++
-      update()
+      count++;
+      count2++;
+      count3++;
+      count4++;
+      update();
     } else {
-      clearInterval(interval)
+      clearInterval(interval);
     }
-  }, 40)
+  }, 40);
+}
+function update() {
+  counter.textContent = count;
+  counter2.textContent = count2;
+  counter3.textContent = count3;
+  counter4.textContent = count4;
 }
 
-window.addEventListener('scroll', isScroll)
+function isScroll() {
+  if (!scrolling && window.scrollY >= 200) {
+    scrolling = true;
+    Counter();
+  }
+}
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 1350) {
+    nav.classList.add("navs");
+    nav.classList.remove("menu");
+    main.classList.remove("blurBg");
+  }
+});
